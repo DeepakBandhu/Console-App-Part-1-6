bc3f828acaefbc014645d992616ec97853996305
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-14 03:42:25 -0700

    Add files via upload

---
+++ Part 6/Part 6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> carBrands = new List<string>() { "Ford", "Chrysler", "Toyota", "Ford", "Ford", "Nissan", "Nissan" };
+
+            foreach(string brand in carBrands)
+            {
+                Console.WriteLine("Brands: " + brand);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
