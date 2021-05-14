db33bded81f7b783de2764d491af3857d589daa1
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-13 21:00:44 -0700

    Add files via upload

---
+++ Part 2/Part 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Hello, World!");
+                break;// this will stop the loop from printing infinitely 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
