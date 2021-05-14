8ccf4980effb27287a594c49dcc1963467598392
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-13 20:50:48 -0700

    Add files via upload

---
+++ Part 1/Part 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Deepak", "Ashely", "Michael", "Brooke" };
+
+            Console.WriteLine("Input whatever text you would like.");
+            string userInput = Console.ReadLine();
+
+            //foreach (string name in names)
+            //{
+            //    string named = names + userInput;
+            //}
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i] + " " +userInput + ".");
+            }
+            Console.ReadLine();
+        }
+        
+
+    }
+}
