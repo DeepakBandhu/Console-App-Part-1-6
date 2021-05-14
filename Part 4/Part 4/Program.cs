9b3272bb74332b42c5568d751c5b1e2bcdab9706
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-13 23:56:33 -0700

    Add files via upload

---
+++ Part 4/Part 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a fruit to find it's index (Apple, Kiwi, Tomato, Orange, Dragon Fruit).");
+            string fruitSearch = Console.ReadLine();
+
+            List<string> fruits = new List<string>() { "Apple", "Orange", "Dragon Fruit", "Tomato", "Kiwi" };
+            bool matchFound = false;
+            foreach (string fruit in fruits)
+            {
+                
+                if (fruit == fruitSearch)
+                {
+                    matchFound = true;
+                    Console.WriteLine(fruits.IndexOf(fruit));
+                    break;
+                }
+
+            }
+            if (matchFound == false)
+            {
+                Console.WriteLine("No match, try again.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
