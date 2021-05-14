0bc872b66bfedcc3175f17e7d496b997ebcb7fb3
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-13 21:11:57 -0700

    Add files via upload

---
+++ Part 3/Part 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Our contestants ages:\n");
+            int[] ages = { 8, 9, 25, 17, 82, 34, 72, 21, 102 };
+
+            for (int i = 0; i < ages.Length; i++)
+            {
+                if (ages[i] > 12)
+                {
+                    Console.WriteLine("Ages of contestants: " + ages[i]);
+                }
+            }
+            Console.WriteLine("\nHere are some of their scores:\n");
+            int[] playerScores = { 98, 80, 85, 73, 82, 55, 49, 50, 94 };
+
+            for (int i = 0; i < playerScores.Length; i++)
+            {
+                if (playerScores[i] >= 50)
+                {
+                    Console.WriteLine("Passing scores of some of the contestants: " + playerScores[i]);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
