f4fc85cfb9fc454ceccefee08f99bc9038589ec6
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-14 22:28:29 -0700

    Add files via upload

---
+++ Part 6/Part 6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> carBrands = new List<string>() { "Ford", "Chrysler", "Toyota", "Ford", "Nissan", "Nissan" };
+            //the repeateBrand list is the empty list that will have the duplicates entered into it
+            List<string> repeatBrand = new List<string>() { };
+
+            foreach(string brand in carBrands)
+            {
+                bool one = true;
+              
+                for(int i = 0; i < repeatBrand.Count; i++)
+                {//this loop will iterate through the duplicate list
+                    
+                    if(repeatBrand[i] == brand)
+                    {
+                        Console.WriteLine(brand + " is a duplicate");
+                        one = false;
+                    }
+
+                }
+                if(one == true)
+                {// this will output what is a duplicate
+                    Console.WriteLine("Brands: " + brand);
+                }
+
+                repeatBrand.Add(brand);
+
+                one = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
